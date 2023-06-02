73a0e12ebbc3385ebf6c182d0068a0802b3b9e6a
ElectroCERP <carloscer27r@gmail.com>
2023-06-01 21:13:39 -0300

    Proyecto Integrador Terminado

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,6 @@ server.use(express.json());
 server.use(cors())
 server.use('/rickandmorty', router);
 
-
 //Para toda aquella ruta que no tenga /rickandmorty dara un error 404 Not Found
 server.use("*",(req,res)=>{
     res.status(404).json({error:'Not Found'})
@@ -23,7 +22,7 @@ server.use("*",(req,res)=>{
 
 server.listen(PORT, async()=> {
     console.log('Server running at port: ' + PORT)
-    await conn.sync({force:false})
+    await conn.sync({alter:true})
     console.log('Entramos a la DB')
 });
 
